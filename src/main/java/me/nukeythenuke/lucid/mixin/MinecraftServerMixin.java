353e73080b9710ce590056a5b208e9d8b2636250
nukeythenuke <nukeythenuke@gmail.com>
2020-11-10 21:57:08 +0000

    Handle spectators correctly

--- src/main/java/me/nukeythenuke/lucid/mixin/MinecraftServerMixin.java
+++ src/main/java/me/nukeythenuke/lucid/mixin/MinecraftServerMixin.java
@@ -23,12 +23,15 @@ public abstract class MinecraftServerMixin {
     @Shadow public abstract GameRules getGameRules();
 
     // Tick all worlds 9 more times if they have at least 1 player and all players are sleeping
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;tickWorlds(Ljava/util/function/BooleanSupplier;)V", shift = At.Shift.AFTER))
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;tickWorlds(Ljava/util/function/BooleanSupplier;)V", shift = At.Shift.AFTER)) // After vanilla ticks worlds
     private void tickWarp(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
         if (!playerManager.getPlayerList().isEmpty() && getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE)) { // Don't do anything if there are no players on the server or doDaylightCycle is false
             getWorlds().forEach((ServerWorld world) -> {
                 List<ServerPlayerEntity> players = world.getPlayers();
-                if (!players.isEmpty() && players.stream().allMatch(LivingEntity::isSleeping)) {
+                if (!players.isEmpty() && // World has players
+                        players.stream().anyMatch(LivingEntity::isSleeping) && // At least 1 player is sleeping
+                        players.stream().allMatch((ServerPlayerEntity player) -> player.isSleeping() || player.isSpectator()) // Every player is either sleeping or a spectator
+                ) {
                     for (int i = 0; i < 9; ++i) {
                         world.tick(shouldKeepTicking);
                     }
