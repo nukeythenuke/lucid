a3c7a4e01e6b5b08663e382ea8edab1f53a0e3f1
nukeythenuke <nukeythenuke@gmail.com>
2020-11-10 21:21:32 +0000

    Do nothing if doDaylightCycle is false

--- src/main/java/me/nukeythenuke/lucid/mixin/MinecraftServerMixin.java
+++ src/main/java/me/nukeythenuke/lucid/mixin/MinecraftServerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,10 +20,12 @@ public abstract class MinecraftServerMixin {
     @Shadow private PlayerManager playerManager;
     @Shadow public abstract Iterable<ServerWorld> getWorlds();
 
+    @Shadow public abstract GameRules getGameRules();
+
     // Tick all worlds 9 more times if they have at least 1 player and all players are sleeping
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;tickWorlds(Ljava/util/function/BooleanSupplier;)V", shift = At.Shift.AFTER))
     private void tickWarp(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-        if (!playerManager.getPlayerList().isEmpty()) { // Don't do anything if there are no players on the server
+        if (!playerManager.getPlayerList().isEmpty() && getGameRules().getBoolean(GameRules.DO_DAYLIGHT_CYCLE)) { // Don't do anything if there are no players on the server or doDaylightCycle is false
             getWorlds().forEach((ServerWorld world) -> {
                 List<ServerPlayerEntity> players = world.getPlayers();
                 if (!players.isEmpty() && players.stream().allMatch(LivingEntity::isSleeping)) {
